d471a500661c5e8d1ea4a9d35b5a15d0f6200797
nidhi334 <knidhibharti8178@gmail.com>
2025-03-21 14:35:15 +0530

    add put method in kitchen model

--- foodapp/kitchen/views.py
+++ foodapp/kitchen/views.py
@@ -17,6 +17,27 @@ class KitchenViewSet(viewsets.ModelViewSet):
     serializer_class = KitchenSerializer
     queryset= Kitchen.objects.all()
 
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response(
+            {"message": "Kitchen created successfully", "data": response.data},
+            status=status.HTTP_201_CREATED
+        )
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        return Response(
+            {"message": "Kitchen updated successfully", "data": response.data},
+            status=status.HTTP_200_OK
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        super().destroy(request, *args, **kwargs)
+        return Response(
+            {"message": "Kitchen deleted successfully"},
+            status=status.HTTP_200_OK
+        )
+
 class BankViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = BankSerializer
