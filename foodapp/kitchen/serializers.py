42da00a8403036f7cf02000601bb3840f7820874
nidhi334 <knidhibharti8178@gmail.com>
2025-03-22 14:54:53 +0530

    represent user in owner model

--- foodapp/kitchen/serializers.py
+++ foodapp/kitchen/serializers.py
@@ -1,10 +1,24 @@
 from rest_framework import serializers
 from kitchen.models import *
 
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["id", "username", "email", "role"]  
+
 class OwnerSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all(), required=False)
     class Meta:
         model = Owner
         fields = "__all__"
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['user'] = CustomUserSerializer(instance.user).data
+        return representation
+
 
 class KitchenSerializer(serializers.ModelSerializer):
     owner = serializers.PrimaryKeyRelatedField(queryset=Owner.objects.all()) 
