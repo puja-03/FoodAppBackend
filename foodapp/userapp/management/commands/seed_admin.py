77b3f386cd4e5ca8ec0fef690f0e48552bbf63eb
nidhi334 <knidhibharti8178@gmail.com>
2025-03-28 23:30:31 +0530

    add admin seeder

---
+++ foodapp/userapp/management/commands/seed_admin.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from userapp.models import CustomUser
+
+class Command(BaseCommand):
+    help = "Seeds an admin user"
+
+    def handle(self, *args, **kwargs):
+        email ="admin@gmail.com"
+        username = "admin"
+        password = "admin123"
+
+        if not CustomUser.objects.filter(email=email).exists():
+            admin_user = CustomUser.objects.create_superuser(email=email, username=username, password=password)
+            self.stdout.write(self.style.SUCCESS(f"Admin user created: {admin_user.email}"))
+        else:
+            self.stdout.write(self.style.WARNING("Admin user already exists."))
