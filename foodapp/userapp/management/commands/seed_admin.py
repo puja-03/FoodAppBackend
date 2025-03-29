1abd7bdfb1a7dee70a008005ae92defd230a8fff
KomalKumari17 <komalydv2760@gmail.com>
2025-03-29 13:20:30 +0530

    admin login done

--- foodapp/userapp/management/commands/seed_admin.py
+++ foodapp/userapp/management/commands/seed_admin.py
@@ -8,9 +8,10 @@ class Command(BaseCommand):
         email ="admin@gmail.com"
         username = "admin"
         password = "admin123"
+        is_verified = True
 
         if not CustomUser.objects.filter(email=email).exists():
-            admin_user = CustomUser.objects.create_superuser(email=email, username=username, password=password)
+            admin_user = CustomUser.objects.create_superuser(email=email, username=username, password=password, is_verified=is_verified)
             self.stdout.write(self.style.SUCCESS(f"Admin user created: {admin_user.email}"))
         else:
             self.stdout.write(self.style.WARNING("Admin user already exists."))
