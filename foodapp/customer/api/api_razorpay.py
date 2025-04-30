c8b3fd3487b7dec6004aa0d34e8b8c737b8d06f2
puja-03 <pujakumari11th2003@gmail.com>
2025-04-30 16:33:54 +0530

    Implement payment verification: add verify_payment function and TransactionAPIView for handling payment transactions; update URLs to include the new endpoint.

--- foodapp/customer/api/api_razorpay.py
+++ foodapp/customer/api/api_razorpay.py
@@ -26,3 +26,52 @@ class RazorpayOrderView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+def verify_payment(self , razorpay_order_id, razorpay_payment_id, razorpay_signature):
+    try:
+        return client.utityy.verify_payment_signature({
+            'razorpay_order_id': razorpay_order_id,
+            'razorpay_payment_id': razorpay_payment_id,
+            'razorpay_signature': razorpay_signature
+        })
+    except Exception as e:
+        raise ValidationError({
+            "status_code": status.HTTP_400_BAD_REQUEST,
+            "message": str(e),
+        })
+   
+
+class TransactionAPIView(APIView):
+    def post(self, request):
+        serializer = TransactionSerializer(data=request.data)
+        if serializer.is_valid():
+            user = request.user
+            payment_id = request.data.get('razorpay_payment_id')
+            order_id = request.data.get('razorpay_order_id')
+            signature = request.data.get('razorpay_signature')
+            amount = request.data.get('amount')
+
+            try:
+                rz_client.verify_payment(
+                    razorpay_order_id=order_id,
+                    razorpay_payment_id=payment_id,
+                    razorpay_signature=signature
+                )
+            except ValidationError as e:
+                return Response(e.detail, status=status.HTTP_400_BAD_REQUEST)
+
+            # Create a transaction record
+            transaction = Transaction.objects.create(
+                user=user,
+                payment_id=payment_id,
+                order_id=order_id,
+                signature=signature,
+                amount=amount
+            )
+            response = {
+                "status_code": status.HTTP_200_OK,
+                "message": "Transaction created successfully",
+                "data": TransactionSerializer(transaction).data
+            }
+            return Response(response, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- foodapp/customer/api/urls.py
+++ foodapp/customer/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .api_razorpay import RazorpayOrderView
+from .api_razorpay import RazorpayOrderView, TransactionAPIView
 
 urlpatterns = [     
     path('order/', RazorpayOrderView.as_view(), name='razorpay_order'),
-    # path('verify_payment/', TransactionAPIView.as_view(), name='verify_payment'),
+    path('verify_payment/', TransactionAPIView.as_view(), name='verify_payment'),
 ]
